6a76a1296c988d9e449e4e2fa7dbfd5c648e93be
dyaksaa <diasnour0395@gmail.com>
2022-05-09 10:46:15 +0700

    range channel

--- unit-testing/helper/channel_test.go
+++ unit-testing/helper/channel_test.go
@@ -2,6 +2,7 @@ package helper
 
 import (
 	"fmt"
+	"strconv"
 	"testing"
 	"time"
 )
@@ -69,5 +70,22 @@ func TestBufferedChannel(t *testing.T) {
 		fmt.Println(<-channel)
 	}()
 	time.Sleep(2 * time.Second)
+	fmt.Println("Selesai")
+}
+
+func TestRangeChannel(t *testing.T) {
+	channel := make(chan string)
+
+	go func() {
+		for i := 0; i < 10; i++ {
+			channel <- "Perulangan ke " + strconv.Itoa(i)
+		}
+		close(channel)
+	}()
+
+	for data := range channel {
+		fmt.Println("Menerima data " + data)
+	}
+
 	fmt.Println("Selesai")
 }
