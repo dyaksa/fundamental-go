ef54611aad856ad25d5d30939e2413e47c2aef03
dyaksaa <diasnour0395@gmail.com>
2022-05-09 15:44:15 +0700

    select channel

--- unit-testing/helper/channel_test.go
+++ unit-testing/helper/channel_test.go
@@ -89,3 +89,34 @@ func TestRangeChannel(t *testing.T) {
 
 	fmt.Println("Selesai")
 }
+
+func TestSelectChannel(t *testing.T) {
+	channel1 := make(chan string)
+	channel2 := make(chan string)
+	defer close(channel1)
+	defer close(channel2)
+
+	go func() {
+		channel1 <- "Dyaksa"
+		channel2 <- "Jauharuddin"
+	}()
+
+	go func() {
+		counter := 0
+		for {
+			select {
+			case data := <-channel1:
+				fmt.Println("data ke ", data)
+				counter++
+			case data := <-channel2:
+				fmt.Println("data ke ", data)
+				counter++
+			}
+			if counter == 2 {
+				break
+			}
+		}
+	}()
+
+	time.Sleep(2 * time.Second)
+}
