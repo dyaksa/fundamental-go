a78de7a6a483f76f816ffb689da33c420c0f7c54
dyaksaa <diasnour0395@gmail.com>
2022-05-09 10:37:26 +0700

    buffered channel

---
+++ unit-testing/helper/channel_test.go
@@ -0,0 +1,73 @@
+package helper
+
+import (
+	"fmt"
+	"testing"
+	"time"
+)
+
+func TestCreateChannel(t *testing.T) {
+	channel := make(chan string)
+	defer close(channel)
+
+	go func() {
+		time.Sleep(2 * time.Second)
+		channel <- "Dyaksa Jauharuddin Nour"
+		fmt.Println("selesai mengisi data channel")
+	}()
+
+	data := <-channel
+	fmt.Println(data)
+	time.Sleep(5 * time.Second)
+}
+
+func TestChannelAsParameter(t *testing.T) {
+	channel := make(chan string)
+	defer close(channel)
+	go GiveMeResponse(channel)
+
+	data := <-channel
+	fmt.Println(data)
+	time.Sleep(5 * time.Second)
+}
+
+func GiveMeResponse(channel chan string) {
+	time.Sleep(2 * time.Second)
+	channel <- "Dyaksa Jauharuddin Nour"
+	fmt.Println("selesai mengisi data dalam channel")
+}
+
+func OnlyIn(channel chan<- string) {
+	time.Sleep(2 * time.Second)
+	channel <- "Dyaksa Jauharuddin Nour"
+}
+
+func OnlyOut(channel <-chan string) {
+	data := <-channel
+	fmt.Println(data)
+}
+
+func TestOnlyInOutChannel(t *testing.T) {
+	channel := make(chan string)
+	defer close(channel)
+	go OnlyIn(channel)
+	go OnlyOut(channel)
+	time.Sleep(3 * time.Second)
+}
+
+func TestBufferedChannel(t *testing.T) {
+	channel := make(chan string, 3)
+	defer close(channel)
+
+	go func() {
+		channel <- "Dyaksa"
+		channel <- "Jauharuddin"
+		channel <- "Nour"
+	}()
+
+	go func() {
+		fmt.Println(<-channel)
+	}()
+	time.Sleep(2 * time.Second)
+	fmt.Println("Selesai")
+}
